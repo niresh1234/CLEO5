356bd3d354500a4c1704da5529c9794852cfb92b
53 <167388310+niresh1234@users.noreply.github.com>
2025-07-18 21:08:34 +0530

    Update stdafx.h

--- source/stdafx.h
+++ source/stdafx.h
@@ -70,8 +70,10 @@ static std::string GetUserDirectory() // already stored in Filepath_User
     return std::move(path);
 }
 
+inline const std::string Filepath_CLEO_ASI = FS::current_path().string();
 inline const std::string Filepath_Game = GetGameDirectory();
 inline const std::string Filepath_User = GetUserDirectory();
-inline const std::string Filepath_Cleo = Filepath_Game + "\\cleo";
+inline const std::string Filepath_Cleo = Filepath_CLEO_ASI + "\\cleo";
+inline const std::string Filepath_RootCleo = Filepath_Game + "\\cleo";
 inline const std::string Filepath_Config = Filepath_Cleo + "\\.cleo_config.ini";
-inline const std::string Filepath_Log = Filepath_Game + "\\cleo.log";
+inline const std::string Filepath_Log = Filepath_Cleo + "\\.cleo.log";
