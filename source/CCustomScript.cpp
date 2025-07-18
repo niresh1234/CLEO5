37ee0e2e5cfba6b3ec699056f65b5232b9b9b3b0
53 <167388310+niresh1234@users.noreply.github.com>
2025-07-18 21:35:14 +0530

    Update CCustomScript.cpp

--- source/CCustomScript.cpp
+++ source/CCustomScript.cpp
@@ -316,7 +316,7 @@ std::string CCustomScript::ResolvePath(const char* path, const char* customWorkD
         case VPref::User: resolved = Filepath_User; break;
         case VPref::Script: resolved = GetScriptFileDir(); break;
         case VPref::Game: resolved = Filepath_Game; break;
-        case VPref::Cleo: resolved = Filepath_Cleo; break;
+        case VPref::Cleo: resolved = Filepath_RootCleo; break;
         case VPref::Modules: resolved = Filepath_Cleo + "\\cleo_modules"; break;
         default: resolved = "<error>"; break; // should never happen
     }
