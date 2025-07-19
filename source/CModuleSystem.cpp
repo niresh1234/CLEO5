85014949ec6c37ab1e2560a8bda67d3b07d59a03
53 <167388310+niresh1234@users.noreply.github.com>
2025-07-19 09:15:55 +0530

    Update CModuleSystem.cpp

--- source/CModuleSystem.cpp
+++ source/CModuleSystem.cpp
@@ -62,7 +62,7 @@ bool CModuleSystem::LoadDirectory(const char* path)
 
 bool CModuleSystem::LoadCleoModules()
 {
-	const auto path = FS::path(Filepath_Cleo).append("cleo_modules").string();
+	const auto path = FS::path(Filepath_CLEO_REL).append("cleo_modules").string();
 	return LoadDirectory(path.c_str());
 }
 
