213b60b957ecf6c2c00eadecea948c3ae34386bf
53 <167388310+niresh1234@users.noreply.github.com>
2026-03-05 18:03:00 +0530

    Update Cleo directory path in stdafx.h

--- source/stdafx.h
+++ source/stdafx.h
@@ -88,7 +88,7 @@ inline void UpdateUserDirectoryPath()
 
 inline const std::string& GetCleoDirectory()
 {
-    static std::string path = GetGameDirectory() + "\\cleo";
+    static std::string path = GetGameDirectory() + "\\scripts\\_CLEO5DIR\\cleo";
     return path;
 }
 
